4dbe6ebc50e8c3dee31a1e1d3aaf8e1b1c51932a
Shahbaz Kamil <72928046+shahbazthedev@users.noreply.github.com>
2020-12-21 18:34:59 -0800

    Blue Aplos minimal design

---
+++ app.js
@@ -0,0 +1,16 @@
+const scrolltp = document.querySelector("#scrolltp");
+scrolltp.addEventListener("click", function(){
+    window.scrollTo({
+        top:0,
+        left:0,
+        behavior:"smooth",
+    });
+});
+
+window.addEventListener ("scroll" , function (){
+    if(window.scrollY >= 700) {
+        scrolltp.style.opacity =1;
+    }else {
+        scrolltp.style.opacity = 0;
+    }
+})
